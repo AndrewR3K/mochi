57f10882b00610e83a4ee2239dd017ad3c588fd8
R3K <280272620+AndrewR3K-AI@users.noreply.github.com>
2026-04-30 01:51:11 -0700

    Register demo components

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,14 @@
 import DefaultTheme from 'vitepress/theme';
 import HomePage from './components/HomePage.vue';
+import DemoFrame from './components/DemoFrame.vue';
+import DemoGallery from './components/DemoGallery.vue';
 import './custom.css';
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('HomePage', HomePage);
+    app.component('DemoFrame', DemoFrame);
+    app.component('DemoGallery', DemoGallery);
   },
 };
